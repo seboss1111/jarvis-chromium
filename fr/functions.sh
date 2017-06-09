3f31d1ee8758c87e43bb2539f5bfa7c578f2f9fe
Rémi M <erim32000@gmail.com>
2017-06-09 18:35:44 +0200

    init

---
+++ fr/functions.sh
@@ -0,0 +1,15 @@
+jv_plugin_affichage_display () {
+ export DISPLAY=":0.0"
+}
+
+jv_plugin_affichage_ouvrir_url (){
+	jv_plugin_affichage_display
+	killall  chromium-browser >& /dev/null >1 /dev/null
+	chromium-browser '-kiosk' "$1" & 
+    site=`echo "$1"`
+}
+
+
+jv_plugin_affichage_fermer_naviguateur (){
+	killall chromium-browser
+}
