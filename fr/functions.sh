ecedf8816d1b792f40ef294714b3e28103754ee9
sebogoss11 <seboss1111@gmail.com>
2017-07-07 22:28:07 +0200

    Update functions.sh

--- fr/functions.sh
+++ fr/functions.sh
@@ -1,9 +1,4 @@
-jv_plugin_affichage_display () {
- export DISPLAY=":0.0"
-}
-
 jv_plugin_affichage_ouvrir_url (){
-	jv_plugin_affichage_display
 	killall  chromium-browser >& /dev/null >1 /dev/null
 	chromium-browser '-kiosk' "$1" & 
     site=`echo "$1"`
@@ -16,7 +11,6 @@ jv_plugin_affichage_fermer_naviguateur (){
 
 
 jv_plugin_affichage_message(){
-	jv_plugin_affichage_display
 	killall  chromium-browser >& /dev/null >1 /dev/null
 	chromium-browser '-kiosk' "localhost/jarvis-chromium-message.php?message=$1" & 
     echo "Affichage du message $1"
